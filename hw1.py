a75788354c995a4df8a22f473531f74dcdfb2eb2
yakupbulbul <81179438+yakupbulbul@users.noreply.github.cod>
2023-04-05 23:50:53 +0300

    readme

--- hw1.py
+++ hw1.py
@@ -1,20 +1,28 @@
+# Importing libraries
 from sklearn.datasets import load_iris
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-# Step 1: Load the dataset
-iris = load_iris()
-X = pd.DataFrame(iris.data, columns=iris.feature_names)
+# Step 1 Loading iris dataset
+iris_data = load_iris()
 
-# Step 2: Standardization
+# Convert the data to pandas dataframe with the feature names as column headers
+iris_df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+
+# Step 2 Standardization
+# Standardize the data to have ean 0 and variance 1
 scaler = StandardScaler()
-X_std = scaler.fit_transform(X)
+
+iris_df_std = scaler.fit_transform(iris_df)
 
 # Step 3: Covariance Matrix
-covariance_matrix = np.cov(X_std.T)
+# Calculate the covariance matrix of the standardized data
+covariance_matrix = np.cov(iris_df_std.T)
+
 
 # Step 4: Eigenvectors and Eigenvalues
+# Calculate the eigenvectors and eigenvalues of the covariance matrix
 eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
 
 # Step 5: Principal Components
@@ -23,12 +31,14 @@ sorted_index = eigenvalues.argsort()[::-1]
 sorted_eigenvalues = eigenvalues[sorted_index]
 sorted_eigenvectors = eigenvectors[:, sorted_index]
 
-# Select the top k eigenvectors
-k = 2
-top_k_eigenvectors = sorted_eigenvectors[:, :k]
+
+# Select the top n eigenvectors, which correspond to the n principal components
+n = 2
+top_k_eigenvectors = sorted_eigenvectors[:, :n]
 
 # Step 6: Projection
-X_pca = X_std.dot(top_k_eigenvectors)
+# Project the standardized data onto the selected principal components to obtain the reduced dataset
+iris_df_pca = iris_df_std.dot(top_k_eigenvectors)
 
 # Print the reduced dataset
-print(X_pca)
+print(iris_df_pca)
